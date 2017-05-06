851426324e00e3bd165da9912143145ca9aabc62
zhangchao <zhangchao_1125@sian.cn>
2017-05-06 13:45:00 +0800

    调整函数定义位置

--- iplookup.c
+++ iplookup.c
@@ -128,7 +128,9 @@ ZEND_METHOD(IpLookUp,total_ip_num)
 
     totalIpNum = zend_read_property(iplookup_ce,getThis(),"totalIpNum",sizeof("totalIpNum")-1,0 TSRMLS_CC);
 
-    RETURN_LONG(Z_STRVAL_P(totalIpNum);
+    RETVAL_LONG(Z_STRVAL_P(totalIpNum);
+    return;
+    //RETURN_LONG(Z_STRVAL_P(totalIpNum);
 }
 
 ZEND_METHOD(IpLookUp,get_index)
